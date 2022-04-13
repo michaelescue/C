a4baf61ac4128ef8918e35f101b6a71fe84015b1
Your Name <mescue@pdx.edu>
2022-04-13 09:57:12 -0700

    typos

--- linkedlist/removeNthFromEnd.c
+++ linkedlist/removeNthFromEnd.c
@@ -8,39 +8,45 @@ unsigned long long count = 0;
 
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     
-    if(head->next == NULL){
-        
-        count = 0;
-        
-        #ifdef DEBUG
-          printf("NULL. Count: %d, Val: %d, Head: %x, Next: %x\n", count, head->val, head, head->next);
-        #endif
-        
-        count++;
-    }
-    else{
-                
-        head->next = removeNthFromEnd(head->next,n);
-        
-        #ifdef DEBUG
-          printf("RETURN. Next: %x\n",head->next);
-        #endif
+  // Switch Conditions for function re-entry.
 
-        count++;
-    }
-    
-    printf("Count: %d, Val: %d, Head: %x, Next: %x\n", count, head->val, head, head->next);
+  if(head->next == NULL){
+      
+      count = 0;
+      
+      #ifdef DEBUG
+        printf("NULL. Count: %d, Val: %d, Head: %x, Next: %x\n", count, head->val, head, head->next);
+      #endif
+      
+      count++;
+  }
+  else{
+              
+      head->next = removeNthFromEnd(head->next,n);
+      
+      #ifdef DEBUG
+        printf("RETURN. Next: %x\n",head->next);
+      #endif
+
+      count++;
+  }
   
-    if(n == count){
-        
-        #ifdef DEBUG
-          printf("%d == %d Head: %x, Next: %x\n", n, count, head, head->next);
-        #endif
-        
-        return head->next;
-    }
-    else{
-        return head;
-    }
+  // Switch Conditions for return value.
+
+  #ifdef DEBUG
+    printf("Count: %d, Val: %d, Head: %x, Next: %x\n", count, head->val, head, head->next);
+  #endif
+  
+  if(n == count){
+      
+      #ifdef DEBUG
+        printf("%d == %d Head: %x, Next: %x\n", n, count, head, head->next);
+      #endif
+
+      return head->next;
+  }
+  else{
+      return head;
+  }
 }
 
