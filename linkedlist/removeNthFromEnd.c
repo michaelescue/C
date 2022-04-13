3380ae22a8bd05213ec3b1482ca8e09fb0b5a01b
Your Name <mescue@pdx.edu>
2022-04-13 10:03:12 -0700

    notes added.

--- linkedlist/removeNthFromEnd.c
+++ linkedlist/removeNthFromEnd.c
@@ -1,5 +1,14 @@
-// Definition for singly-linked list
-#include <stdlib.h>
+/**
+ * @file removeNthFromEnd.c
+ * @author Michael Escue
+ * @brief Recursive remove Nth node from end of list algorithm. ! NOT the fastest method. Using loops with temporary pointers can be faster.
+ * @version 0.1
+ * @date 2022-04-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include "ListNode.h"
 
 #define DEBUG
@@ -36,7 +45,7 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
   #ifdef DEBUG
     printf("Count: %d, Val: %d, Head: %x, Next: %x\n", count, head->val, head, head->next);
   #endif
-  
+
   if(n == count){
       
       #ifdef DEBUG
